5caf8d31d70edc1bc81022c30abcbf3f3bb5ee8b
Jeffrey-Wang <944688482@qq.com>
2020-11-15 20:31:34 +0800

    feat: webpack init.

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/excel.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'excel.js',
+  },
+  plugins: [
+  ],
+}
